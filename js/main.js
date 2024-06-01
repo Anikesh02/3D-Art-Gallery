68f09ef06d4fa1b22e15e89638217c6a60d28e77
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-06-01 23:56:50 +0530

    Creating and Animating the cube in website

--- js/main.js
+++ js/main.js
@@ -3,28 +3,42 @@
 const scene = new THREE.Scene();
 
 //Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0,1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 scene.add(camera);
 camera.position.z = 5;   // Move the camera back 5 units so we can see the scene
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({antialias: true});  // For Smooth Edges
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor("0xfffff, 1");  // Set the background color of the scene
+renderer.setClearColor(0xffffff, 1);  // Set the background color of the scene
 document.body.appendChild(renderer.domElement);  // Add the renderer to the body element
 
 // Lights
 
 // Ambient Light
-const ambientLight = new THREE.AmbientLight(0x101010, 1);
-ambientLight.position = camera.position;  //Light follows the camera position
+let ambientLight = new THREE.AmbientLight(0x101010, 1);
+ambientLight.position.copy(camera.position);  //Light follows the camera position
 scene.add(ambientLight);
 
 
 // Directional Light
-const sunLight = new THREE.DirectionalLight(0xddddd, 1);  // color, intensity
+let sunLight = new THREE.DirectionalLight(0xdddddd, 1);  // color, intensity
 sunLight.position.y = 15;
 scene.add(sunLight);
 
-//Renderer
-renderer.render(scene, camera);
+// Geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);  // Width, Height, Depth
+const material = new THREE.MeshBasicMaterial({color: 'blue'});  // Color of the cube
+const cube = new THREE.Mesh(geometry, material);    // Create the cube
+scene.add(cube);    // Add the cube to the scene
+
+
+// Animation
+let render = function() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+render();
