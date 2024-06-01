86aa39058c537f8d238848892fd00a99a7767681
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-06-01 22:58:38 +0530

    Different Types of Lights

--- js/main.js
+++ js/main.js
@@ -7,9 +7,24 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 scene.add(camera);
 camera.position.z = 5;   // Move the camera back 5 units so we can see the scene
 
+// Renderer
 const renderer = new THREE.WebGLRenderer({antialias: true});  // For Smooth Edges
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor("0xfffff, 1");  // Set the background color of the scene
 document.body.appendChild(renderer.domElement);  // Add the renderer to the body element
 
+// Lights
 
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(0x101010, 1);
+ambientLight.position = camera.position;  //Light follows the camera position
+scene.add(ambientLight);
+
+
+// Directional Light
+const sunLight = new THREE.DirectionalLight(0xddddd, 1);  // color, intensity
+sunLight.position.y = 15;
+scene.add(sunLight);
+
+//Renderer
+renderer.render(scene, camera);
